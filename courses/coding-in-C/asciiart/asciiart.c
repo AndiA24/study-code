c8f6328c1071fda45b78fe4a8a3cd4c61b76e477
AndreasAdler <a.adler.tab@icloud.com>
2026-01-20 17:45:51 +0100

    Added asciiart folder - did task

---
+++ courses/coding-in-C/asciiart/asciiart.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void){
+
+    printf("       .---.\n");
+    printf("      /     \\\n");
+    printf("      \\.@-@./\n");
+    printf("      /`\\_/`\\\n");
+    printf("     //  _  \\\\\n");
+    printf("    | \\     )|_\n");
+    printf("   /`\\_`>  <_/ \\\n");
+    printf("   \\__/'---'\\__/\n");
+}
