e0c45c19b9ddaf82fdbb5fc78f308a3e6cf9dd18
AndreasAdler <a.adler.tab@icloud.com>
2026-02-04 11:52:05 +0100

    Fix Bugs in calc Script

--- courses/coding-in-C/Lab_3/mysolution/calc.c
+++ courses/coding-in-C/Lab_3/mysolution/calc.c
@@ -10,34 +10,38 @@ int main(){
         char opperator = 0;
         printf("------------------C-Console Calculator------------------\n");
         printf("Please chose the Opperator you want to use:\nAddition: +\n Subtraction: -\n Multiplication: *\n Division: /\n\n");
-        if (scanf("%c", &opperator) == 1){
+        if (scanf(" %c", &opperator) == 1){
             if(!(opperator == '+' || opperator == '-' || opperator == '*' || opperator == '/')){
                 printf("Ungültige Eingabe: Es sind nur die Opperatoren '+', '-', '*' und '/' erlaubt\n");                
             }
-        }
-        if (opperator != 0){
-            printf("Please enter the first Number: ");
-            scanf("%f", &firstNum);
-            printf("Please enter the second Number: ");
-            if(scanf("%f", &secondNum) == 1){
-                if(opperator = '/' && (secondNum == 0)){
-                    printf("You cant divide a Number by zero.\n");
+            else{
+                while (getchar() != '\n');
+                if (opperator != 0){
+                    printf("Please enter the first Number: ");
+                    scanf("%f", &firstNum);
+                    while (getchar() != '\n');
+                    printf("Please enter the second Number: ");
+                    if(scanf("%f", &secondNum)){
+                        if(opperator == '/' && (secondNum == 0)){
+                            printf("You cant divide a Number by zero.\n");
+                        }
+                        else{
+                            if(opperator == '+'){
+                                result = firstNum + secondNum;
+                            }
+                            if(opperator == '-'){
+                                result = firstNum - secondNum;
+                            }
+                            if(opperator == '*'){
+                                result = firstNum * secondNum;
+                            }
+                            if(opperator == '/'){
+                                result = firstNum / secondNum;
+                            }
+                            printf("The Result of your Calculation is: %f %c %f = %f\n", firstNum, opperator, secondNum, result);
+                        }
+                    }
                 }
-                else{
-                    if(opperator == '+'){
-                        result = firstNum + secondNum;
-                    }
-                    if(opperator == '-'){
-                        result = firstNum - secondNum;
-                    }
-                    if(opperator == '*'){
-                        result = firstNum * secondNum;
-                    }
-                    if(opperator == '/'){
-                        result = firstNum / secondNum;
-                    }
-                }
-                printf("The Result of your Calculation is: %f\n", result);
             }
         }
     }
