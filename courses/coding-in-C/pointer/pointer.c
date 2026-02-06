3fe89dc62f12f3b413140123f4c8ccedd5a8b651
AndreasAdler <a.adler.tab@icloud.com>
2026-02-06 14:35:44 +0100

    Add calculator Task

---
+++ courses/coding-in-C/pointer/pointer.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int myNumbers[] = {10,20,30,40,50};
+    int *pNum1 = myNumbers;
+    pNum1 ++;
+    int *pNum2 = pNum1 + 3;
+    int diff = pNum2 - pNum1;
+
+    printf("Adresse von Element 1: %p\n", pNum1);
+    printf("Adresse von Element 2: %p\n", pNum2);
+    printf("Adressdifferenz zwischen 2. und 4. Element: %d", diff);
+    return 0;
+}
