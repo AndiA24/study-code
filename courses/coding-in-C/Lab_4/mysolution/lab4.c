4ce1b646a23bb4b547a9e22c3a0bf781644e2887
AndreasAdler <a.adler.tab@icloud.com>
2026-02-11 16:53:05 +0100

    Solve Sec 1 of Lab 4

--- courses/coding-in-C/Lab_4/mysolution/lab4.c
+++ courses/coding-in-C/Lab_4/mysolution/lab4.c
@@ -1,11 +1,29 @@
 #include <stdio.h>
 
+void letter_change(char str[]){
+    *(str + 1) = 'c';
+
+}
+
 int main(){
     char content[] = "Curly bird catches the worm";
-    char *pcontent = &content[0];
+    char *p_content = &content[0];
+
+    char str2[] = "AAtions speak louder than words";
 
     printf("%s\n", content);
-    printf("%s\n", pcontent);
-    printf("%p\n", pcontent);
+    printf("%s\n", p_content);
+    printf("%p\n", p_content);
+
+    *(p_content) = 'E';
+    *(p_content + 1) = 'a';
+
+    printf("%s\n", content);
+
+    printf("\n");
+    printf("%s\n", str2);
+    letter_change(str2);
+    printf("%s\n", str2);
+
 
 }
