28208b10e6f0a7b224efe635c9b3e01260c4f6a4
AndreasAdler <a.adler.tab@icloud.com>
2026-02-18 09:24:31 +0100

    Add referencing via pointer to Sec2

--- courses/coding-in-C/Lab_5/mysolution/Sec2.c
+++ courses/coding-in-C/Lab_5/mysolution/Sec2.c
@@ -2,7 +2,13 @@
 #include <string.h>
 
 int main(int argc, char **argv){
+    if(argc < 2){
+        printf("At least one argument is expected.\n");
+        return 0;
+    }
+
     for(int i = 0; i < argc; i++){
         printf("Argument %d at Adress %p has value '%s' and lenght %ld\n", argc, argv[i], argv[i], strlen(argv[i]));
+        printf("Argument %d at Adress %p has value '%s' and lenght %ld\n", argc, *(argv + i), *(argv + i), strlen(argv[i]));
     }
 }
